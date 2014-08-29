de39407ef51b49153ad4e0f86e8b7b3cf05af87f
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-08-29 23:54:42 +0200

    water, fire, source, volcano, earth & perlin noice

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ C.width = C.height = size * 4;
 var gl = C.getContext("webgl") || C.getContext("experimental-webgl");
 
 var colors = [
-  0, 0, 0, // 0: air
-  190/255, 170/255, 130/255, // 1: ground
-  215/255, 45/255, 20/255, // 2: fire
-  69/255, 160/255, 180/255, // 3: water
-  170/255, 180/255, 180/255 // 4: air
+  0.11, 0.16, 0.23, // 0: air
+  0.74, 0.66, 0.51, // 1: earth
+  0.84, 0.17, 0.08, // 2: fire
+  0.40, 0.80, 0.95, // 3: water
+
+  // spawners
+  0.60, 0.00, 0.00, // 4: volcano
+  0.27, 0.63, 0.70, // 5: source of water
+
+  0.66, 0.71, 0.71  // 6: air (aka wind)
 ];
 
 var shader, shaderSrc, shaderType, program;
@@ -93,12 +98,16 @@ function affectColor (buf, i, c) {
   buf[i+3] = 255;
 }
 
+var perlin = generatePerlinNoise(size, size);
+var perlin2 = generatePerlinNoise(size, size);
+
 var data = new Uint8Array(4 * size * size);
 for(var i = 0; i < data.length; i += 4) {
   var r = Math.floor(Math.random() * colors.length);
   //affectColor(data, i, r);
-  if (i/4 == size * size / 2 - size * 0.5) affectColor(data, i, 1);
-  if (Math.random() < 0.0001) affectColor(data, i, 1);
+  if (perlin[i/4] > 0.55) affectColor(data, i, 1);
+  if (perlin[i/4] > 0.85) affectColor(data, i, 4);
+  if (perlin2[i/4] > 0.8) affectColor(data, i, 5);
 }
 
 var logicTexture = gl.createTexture();
@@ -121,7 +130,7 @@ gl.uniform3fv(logicColorsL, colors);
 var logicProgram = program;
 
 var start = Date.now();
-(function loop () {
+(function update () {
   var time = (Date.now()-start)/1000;
   gl.useProgram(logicProgram);
   gl.uniform1f(logicTimeL, time);
@@ -130,17 +139,92 @@ var start = Date.now();
   gl.bindFramebuffer(gl.FRAMEBUFFER, logicFramebuffer);
   gl.drawArrays(gl.TRIANGLES, 0, 6);
   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  setTimeout(update, 50);
+}());
 
-
+(function render () {
+  var time = (Date.now()-start)/1000;
   gl.useProgram(renderProgram);
   gl.uniform1i(renderLogicL, 0);
   gl.uniform1f(renderTimeL, time);
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.vertexAttribPointer(renderPositionL, 2, gl.FLOAT, false, 0, 0);
   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  setTimeout(loop, 50);
+  requestAnimationFrame(render);
 }());
 
+
+
+function generatePerlinNoise(width, height, options) {
+  options = options || {};
+  var octaveCount = options.octaveCount || 4;
+  var amplitude = options.amplitude || 0.1;
+  var persistence = options.persistence || 0.2;
+  var whiteNoise = generateWhiteNoise(width, height);
+
+  var smoothNoiseList = new Array(octaveCount);
+  var i;
+  for (i = 0; i < octaveCount; ++i) {
+    smoothNoiseList[i] = generateSmoothNoise(i);
+  }
+  var perlinNoise = new Array(width * height);
+  var totalAmplitude = 0;
+  // blend noise together
+  for (i = octaveCount - 1; i >= 0; --i) {
+    amplitude *= persistence;
+    totalAmplitude += amplitude;
+
+    for (var j = 0; j < perlinNoise.length; ++j) {
+      perlinNoise[j] = perlinNoise[j] || 0;
+      perlinNoise[j] += smoothNoiseList[i][j] * amplitude;
+    }
+  }
+  // normalization
+  for (i = 0; i < perlinNoise.length; ++i) {
+      perlinNoise[i] /= totalAmplitude;
+  }
+
+  return perlinNoise;
+
+  function generateSmoothNoise(octave) {
+    var noise = new Array(width * height);
+    var samplePeriod = Math.pow(2, octave);
+    var sampleFrequency = 1 / samplePeriod;
+    var noiseIndex = 0;
+    for (var y = 0; y < height; ++y) {
+      var sampleY0 = Math.floor(y / samplePeriod) * samplePeriod;
+      var sampleY1 = (sampleY0 + samplePeriod) % height;
+      var vertBlend = (y - sampleY0) * sampleFrequency;
+      for (var x = 0; x < width; ++x) {
+        var sampleX0 = Math.floor(x / samplePeriod) * samplePeriod;
+        var sampleX1 = (sampleX0 + samplePeriod) % width;
+        var horizBlend = (x - sampleX0) * sampleFrequency;
+
+        // blend top two corners
+        var top = interpolate(whiteNoise[sampleY0 * width + sampleX0], whiteNoise[sampleY1 * width + sampleX0], vertBlend);
+        // blend bottom two corners
+        var bottom = interpolate(whiteNoise[sampleY0 * width + sampleX1], whiteNoise[sampleY1 * width + sampleX1], vertBlend);
+        // final blend
+        noise[noiseIndex] = interpolate(top, bottom, horizBlend);
+        noiseIndex += 1;
+      }
+    }
+    return noise;
+  }
+}
+function generateWhiteNoise(width, height) {
+  var noise = new Array(width * height);
+  for (var i = 0; i < noise.length; ++i) {
+    noise[i] = Math.random();
+  }
+  return noise;
+}
+function interpolate(x0, x1, alpha) {
+  return x0 * (1 - alpha) + alpha * x1;
+}
+
+
+// Of-course this will go away !
 
 function validate (shader, shaderSource) {
   var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
@@ -166,3 +250,7 @@ function validateProg (program) {
      throw new Error(program+" "+gl.getProgramInfoLog(program));
    }
 }
+
+
+
+
